1f1536d7c0a26f43fca99cc4e7e142c89ab2edf7
Dave <elsh0009@hz.nl>
2022-01-22 19:13:25 +0100

    added player factory to the game
    attacks not yet implemented

---
+++ src/players/Swordsman.ts
@@ -0,0 +1,11 @@
+/// <reference path="../Player.ts"/>
+
+class Swordsman extends Player {
+
+    public constructor() {
+        super();
+        this.healthPoints = 13;
+        this.damage = 15
+        //attacks
+    }
+}
